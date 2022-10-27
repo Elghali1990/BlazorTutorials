75f67707ea5498e02bc3afc3a220434fe8c15f04
Elghali1990 <elghali-mohamed@outlook.fr>
2022-10-27 23:43:45 +0100

    commite

--- Api/Controllers/EmployeeController.cs
+++ Api/Controllers/EmployeeController.cs
@@ -1,4 +1,5 @@
 ﻿using Infrastructer.Interfaces;
+using Infrastructer.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,10 +9,68 @@ namespace Api.Controllers
     [ApiController]
     public class EmployeeController : ControllerBase
     {
-        private readonly IEmployee employee;
+        private readonly IEmployee employeeDb;
         public EmployeeController(IEmployee _employee)
         {
-            employee = _employee;
+            employeeDb = _employee;
+        }
+
+
+        [HttpGet]
+        [Route("getAllEmployees")]
+        public IActionResult getAll(Employee employee)
+        {
+            return Ok(employeeDb.GetAllEmployees());
+        }
+
+        [HttpGet]
+        [Route("getEmployeesById/{id}")]
+        public IActionResult getById(int id)
+        {
+            var emp = employeeDb.GetEmployeeById(id);
+            if (emp is null)
+            {
+                return NotFound("Employee not found");
+            }
+            return Ok(emp);
+        }
+
+        [HttpPost]
+        [Route("create")]
+        public IActionResult create(Employee employee)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Employe data is incorrect");
+            }
+            employeeDb.AddEmployee(employee);
+            return Ok(employee);
+        }
+
+        [HttpDelete]
+        [Route("deleteEmployee/{id}")]
+        public IActionResult delete(int id)
+        {
+            var emp = employeeDb.GetEmployeeById(id);
+            if (emp is null)
+            {
+                return NotFound("Employee not found");
+            }
+            employeeDb.DeleteEmployee(emp);
+            return Ok(emp);
+        }
+
+        [HttpPut]
+        [Route("update/{id}")]
+        public IActionResult update(int id,Employee employee)
+        {
+            var emp = employeeDb.GetEmployeeById(id);
+            if (emp is null)
+            {
+                return NotFound("Employee not found");
+            }
+            employeeDb.UpdateEmployee(emp);
+            return Ok(employee);
         }
 
     }
